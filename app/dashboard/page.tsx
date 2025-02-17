a394a6d57046bb5cc4aa03c31c5a1545773e21f1
contactsingulary <contact@singulary.de>
2025-02-17 20:30:42 +0100

    fix: move 'use client' directive to top of files

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,6 @@
-export const dynamic = 'force-dynamic'
-
 'use client'
+
+export const dynamic = 'force-dynamic'
 
 import { createBrowserClient } from "@supabase/ssr"
 import { ChatWidget } from "@/components/chat-widget"
